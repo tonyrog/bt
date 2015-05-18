0fd4f9bac832152b089b57b07729304c659b829b
Magnus Feuer <mfeuer@jaguarlandrover.com>
2015-05-18 09:09:54 -0700

    Half baked driver code

--- c_src/bt_linux_drv.c
+++ c_src/bt_linux_drv.c
@@ -3,6 +3,8 @@
 //
 
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
@@ -15,6 +17,8 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/l2cap.h>
 #include <bluetooth/rfcomm.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
 
 #include "dthread/include/dlog.h"
 #include "dthread/include/ddata.h"
@@ -29,6 +33,13 @@
 
 #define alloc_type(type) calloc(1, sizeof(type))
 
+typedef struct _drv_data_t {
+    int dev_id;
+    int mgmt_sock;
+    int dev_sock;
+    int inquiry_pid;
+    int inquiry_fd;
+} drv_data_t;
 
 int set_nonblock(int fd)
 {
@@ -46,6 +57,215 @@ int set_nonblock(int fd)
 
 #define ERR_SHORT 1
 #define ERR_LONG  2
+
+
+
+static int mgmt_open(void)
+{
+	struct sockaddr_hci addr;
+	int sk;
+
+	sk = socket(AF_BLUETOOTH, SOCK_RAW, BTPROTO_HCI);
+	if (sk < 0) {
+		fprintf(stderr, "socket: %s\n", strerror(errno));
+		return sk;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.hci_family = AF_BLUETOOTH;
+	addr.hci_dev = HCI_DEV_NONE;
+	addr.hci_channel = HCI_CHANNEL_CONTROL;
+
+	if (bind(sk, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+		fprintf(stderr, "bind: %s\n", strerror(errno));
+		close(sk);
+		return -1;
+	}
+
+	return sk;
+}
+
+
+static inline int get_address(ddata_t* data, bdaddr_t* val)
+{
+    if (ddata_r_avail(data) >= sizeof(bdaddr_t)) {
+	// For some reason, the address is reversed when it
+	// hits bt_linux_drv.
+	val->b[0] = *(data->rd+5);
+	val->b[1] = *(data->rd+4);
+	val->b[2] = *(data->rd+3);
+	val->b[3] = *(data->rd+2);
+	val->b[4] = *(data->rd+1);
+	val->b[5] = *(data->rd+0);
+
+	// Was: memcpy(val, data->rd, sizeof(bdaddr_t));
+	data->rd += sizeof(bdaddr_t);
+	return 1;
+    }
+    return 0;
+}
+
+static inline void ddata_put_addr(ddata_t* data, bdaddr_t* addr)
+{
+    uint8_t* ptr = ddata_alloc(data, sizeof(bdaddr_t)+1);
+    *ptr++ = ADDR;
+    *ptr++ = addr->b[5];
+    *ptr++ = addr->b[4];
+    *ptr++ = addr->b[3];
+    *ptr++ = addr->b[2];
+    *ptr++ = addr->b[1];
+    *ptr++ = addr->b[0];
+}
+
+
+static void bt_local_info(drv_data_t* lctx, ddata_t* data_in, ddata_t* data_out)
+{
+    uint8_t op_code;
+
+    ddata_put_tag(data_out, LIST);
+
+    while(ddata_get_uint8(data_in, &op_code)) {
+	switch(op_code) {
+	case NFO_LOCAL_NAME: {
+	    struct hci_dev_info di;
+
+	    DEBUGF("local_info: local name");
+
+	    hci_devinfo(lctx->dev_id, &di);
+
+	    ddata_put_string(data_out, di.name);
+	    break;
+	}
+
+	case NFO_LOCAL_CLASS: {
+	    uint8_t cls_str[4];	    
+	    uint32_t cls;
+
+	    memset(cls_str, 0, sizeof(cls_str));
+	    // Send in the lowest three bytes of the uint32 to 
+	    // retrieve the three byte class
+	    if (hci_read_class_of_dev(lctx->dev_sock, cls_str+1, 0) == -1) {
+		DEBUGF("hci_read_class_of_dev() failed: %s", strerror(errno));
+		break;
+	    }
+	    
+	    cls = *((int*)cls_str);
+
+	    DEBUGF("local_info: local class: %X", cls);
+	    ddata_put_uint32(data_out, cls);
+	    break;
+	}
+
+	case NFO_LOCAL_ADDRESS: {
+	    struct hci_dev_info di;
+	    DEBUGF("local_info: local address");
+	    hci_devinfo(lctx->dev_id, &di);
+	    ddata_put_addr(data_out, &(di.bdaddr));
+	    break;
+	}
+	    
+	case NFO_LOCAL_DISCOVERABLE: {
+	    // Boolean value;
+	    // if (IOBluetoothLocalDeviceGetDiscoverable(&value) == kIOReturnSuccess)
+	    DEBUGF("local_info: discoverable");
+	    ddata_put_boolean(data_out, 1);
+	    break;
+	}
+
+	case NFO_LOCAL_POWER_STATE: {
+	    /* BluetoothHCIPowerState powerState = */
+	    /* 	[[IOBluetoothHostController defaultController] powerState]; */
+	    /* if (powerState == kBluetoothHCIPowerStateON) */
+	    /* 	ddata_put_atom(data_out, "on"); */
+	    /* else if (powerState == kBluetoothHCIPowerStateOFF) */
+	    /* 	ddata_put_atom(data_out, "off"); */
+	    /* else if (powerState == kBluetoothHCIPowerStateUnintialized) */
+	    /* 	ddata_put_atom(data_out, "unintialized"); */
+	    /* else  */
+	    DEBUGF("local_info: power_state");
+	    ddata_put_atom(data_out, "on");
+
+	    break;
+	}
+	default:
+	    break;
+	}
+    }
+    ddata_put_tag(data_out, LIST_END);
+}
+
+
+static void launch_inquiry_proc(drv_data_t *lctx, uint32_t max_seconds) {
+    inquiry_info *ii = NULL;
+    int32_t max_rsp;
+    int32_t num_rsp;
+    int dev_id, sock, len, flags;
+    int i;
+    int pfd[2];
+    int child_pid;
+
+    pipe(pfd);
+    if ((child_pid = fork()) > 0) {
+	close(pfd[1]);
+	lctx->inquiry_pid = child_pid;
+	lctx->inquiry_fd = pfd[0];
+	return ;
+    }
+
+    // We are child
+    close(pfd[0]);
+	
+    // Open a new socket to the adapter
+    // just so that we don't mess up something in the stack
+    // by accessing the same fd from two different processes.
+    dev_id = hci_get_route(NULL);
+    sock = hci_open_dev( dev_id );
+
+    if (dev_id < 0 || sock < 0) {
+	num_rsp = -1;
+	write(pfd[1], &num_rsp, sizeof(num_rsp));
+	close(pfd[1]);
+	close(sock);
+	exit(0);
+
+    }
+
+    len = (int32_t) ((float) max_seconds / 1.28); // 1.28 second per length unit
+    max_rsp = 255;
+    flags = IREQ_CACHE_FLUSH;
+
+    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
+    
+    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+
+    if( num_rsp < 0 )  {
+	num_rsp = 0;
+	write(pfd[1], &num_rsp, sizeof(num_rsp));
+	close(pfd[1]);
+	free( ii );
+	close( sock );
+	exit(0);
+    }
+
+    bdaddr_t resp[num_rsp];
+
+    // Retrieve address and remote name
+    for (i = 0; i < num_rsp; i++) 
+	resp[i] = (ii+i)->bdaddr;
+
+    // Write number of responses that we have.
+    printf("Num_resp: %d\n", num_rsp);
+    write(pfd[1], &num_rsp, sizeof(num_rsp));
+    write(pfd[1], resp, sizeof(resp));
+
+    close(pfd[1]);
+    free( ii );
+    close( sock );
+    exit(0);
+}
+
+
+
 /* type: 1=short-atom  2=string-long  3=both (encapsule in LIST ot TUPLE) */
 static void ddata_put_io_error(ddata_t* data, int errnum, int type)
 {
@@ -59,11 +279,24 @@ static void ddata_put_io_error(ddata_t* data, int errnum, int type)
     }
 }
 
+
 static void cleanup(subscription_t* s)
 {
     DEBUGF("cleanup: %s", format_subscription(s));
+    drv_data_t* lctx = (drv_data_t*) s->handle;
+
     switch(s->type) {
-    case INQUIRY: break;
+    case INQUIRY: {
+	bt_poll_del(lctx->inquiry_fd);
+	close(lctx->inquiry_fd);
+
+	kill(lctx->inquiry_pid, SIGHUP);
+	lctx->inquiry_pid = -1;
+	lctx->inquiry_fd = -1;
+	wait(0);
+	break;
+    }
+
     case REMOTE_NAME: break;
     case CONNECT: break;
     case SDP_QUERY: break;
@@ -125,15 +358,7 @@ static void send_event(uint32_t sid, const char* evtname)
     ddata_final(&data);
 }
 
-static inline int get_address(ddata_t* data, bdaddr_t* val)
-{
-    if (ddata_r_avail(data) >= sizeof(bdaddr_t)) {
-	memcpy(val, data->rd, sizeof(bdaddr_t));
-	data->rd += sizeof(bdaddr_t);
-	return 1;
-    }
-    return 0;
-}
+
 
 // CALLBACK 
 static void rfcomm_running(struct pollfd* pfd, void* arg)
@@ -141,12 +366,76 @@ static void rfcomm_running(struct pollfd* pfd, void* arg)
     subscription_t* s = arg;
 
     if (pfd->revents & POLLIN) {  // input ready
+	uint8_t buf[1024];
+	uint8_t bt_data[800];
+	int32_t bt_data_len;
+	ddata_t data;
+
 	DEBUGF("rfcomm_running: %d has input", PTR2INT(s->handle));
+
+	bt_data_len = read(pfd->fd, bt_data, sizeof(bt_data));
+	
+	ddata_init(&data, buf, sizeof(buf), 0);
+	ddata_put_UINT32(&data, 0);
+	ddata_put_tag(&data, REPLY_EVENT);
+	ddata_put_UINT32(&data, s->id);
+	ddata_put_tag(&data, TUPLE);
+	ddata_put_atom(&data, "data");
+	ddata_put_binary(&data, bt_data, bt_data_len);
+	ddata_put_tag(&data, TUPLE_END);
+	ddata_send(&data, 1);
+	ddata_final(&data);
     }
 
     if (pfd->revents & POLLOUT) {  // output ready
 	DEBUGF("rfcomm_running: %d may output", PTR2INT(s->handle));
     }
+}
+
+// 
+static void hci_inquiry_result(struct pollfd* pfd, void* arg)
+{
+    subscription_t* s = arg;
+    uint8_t  out_buf[2048];
+    ddata_t data_out;
+
+    DEBUGF("hci_inquiry_result: %d", PTR2INT(s->handle));
+    int32_t count;
+
+
+    // Read count, which may be -1.
+    read(pfd->fd, &count, sizeof(count));
+
+    DEBUGF("hci_inquiry_res: %d addresses\n", count);
+
+    ddata_init(&data_out, out_buf, sizeof(out_buf), 0);
+
+    ddata_put_tag(&data_out, REPLY_OK);
+    ddata_put_UINT32(&data_out, s->cmdid);
+    ddata_put_tag(&data_out, LIST);
+
+    while(count-- > 0) {
+	bdaddr_t resp;
+	read(pfd->fd, &resp, sizeof(resp));
+	ddata_put_addr(&data_out, &resp);
+    }
+
+    ddata_put_tag(&data_out, LIST_END);
+
+    bt_poll_del(pfd->fd);
+    
+    close(pfd->fd);
+
+    // Wait for child process to die.
+    wait(0);
+
+    ddata_send(&data_out, 1);
+    ddata_final(&data_out);
+
+    s->cmdid = 0;
+
+    // only for Inquiry objects
+    release_subscription(s);
 }
 
 // CALLBACK 
@@ -171,6 +460,7 @@ void bt_command(bt_ctx_t* ctx, const uint8_t* src, uint32_t src_len)
     uint8_t  out_buf[2048];
     ddata_t data_in;
     ddata_t data_out;
+    drv_data_t* lctx = (drv_data_t*) ctx->drv_data;
 
     // dump subscription list
     if (dlog_debug_level == DLOG_DEBUG) {
@@ -210,6 +500,7 @@ void bt_command(bt_ctx_t* ctx, const uint8_t* src, uint32_t src_len)
 	goto reply;
     }	
 
+
     case CMD_RFCOMM_OPEN: { /* id:32 bt-address(6) channel-id:8 */
 	uint32_t sid;
 	bdaddr_t bt_addr;
@@ -218,6 +509,7 @@ void bt_command(bt_ctx_t* ctx, const uint8_t* src, uint32_t src_len)
 	int r;
 	uint8_t channel_id;
 	subscription_t* s;
+	char addrstr[40];
 
 	DEBUGF("CMD_RFCOMM_OPEN cmdid=%d", cmdid);
 
@@ -238,6 +530,8 @@ void bt_command(bt_ctx_t* ctx, const uint8_t* src, uint32_t src_len)
 	addr.rc_family = AF_BLUETOOTH;
 	addr.rc_channel = channel_id;
 	bacpy(&addr.rc_bdaddr, &bt_addr);
+	ba2str(&bt_addr, addrstr );
+	DEBUGF("Will connect to %s channel %d", addrstr, channel_id);
 	if (set_nonblock(sock) < 0) {
 	    bt_error = errno;
 	    close(sock);
@@ -250,7 +544,7 @@ void bt_command(bt_ctx_t* ctx, const uint8_t* src, uint32_t src_len)
 		goto bt_error;
 	    }
 	}
-	if ((s = new_subscription(RFCOMM,sid,cmdid,NULL,cleanup)) == NULL) {
+	if ((s = new_subscription(RFCOMM,sid,cmdid,0,cleanup)) == NULL) {
 	    close(sock);
 	    goto mem_error;
 	}
@@ -332,7 +626,11 @@ void bt_command(bt_ctx_t* ctx, const uint8_t* src, uint32_t src_len)
 	    else
 		ddata_add(s->out, data_in.rd, len);
 	    r = write(sock, s->out->rd, ddata_r_avail(s->out));
-	    DEBUGF("CMD_RFCOMM_SEND wrote=%d", r);
+	    if (r != -1)
+		DEBUGF("CMD_RFCOMM_SEND wrote=%d", r);
+	    else
+		DEBUGF("CMD_RFCOMM_SEND failed: %s", strerror(errno));
+		
 	    if ((r >= 0) || ((r < 0) && (errno == EINPROGRESS))) {
 		if (r < 0) r = 0;
 		s->out->rd += r;
@@ -356,6 +654,105 @@ void bt_command(bt_ctx_t* ctx, const uint8_t* src, uint32_t src_len)
 	goto done;
     }
 
+    case CMD_INQUIRY_START: { /* arguments: id:32 secs:32 */
+	uint32_t sid;
+	uint32_t secs;
+
+	subscription_t* s;
+	if (!ddata_get_uint32(&data_in, &sid))
+	    goto badarg;
+
+	if (!ddata_get_uint32(&data_in, &secs))
+	    goto badarg;
+	
+	DEBUGF("CMD_INQUIRY_START: %d", sid); 
+	if (lctx->inquiry_pid != -1) { 
+	    DEBUGF("CMD_INQUIRY_START: Already in progress"); 
+	    goto error; 
+	} 
+
+	// hci_inquiry() is very, very blocking. We need to run it in a separate  
+	// process in order to maintain asynchronicity. 
+	// Will set ctx->drv_data->inquriry_pid and inquiry_fd
+	launch_inquiry_proc(lctx, secs); 
+	DEBUGF("CMD_INQUIRY_START: process launched"); 
+
+	if ((s = new_subscription(INQUIRY,sid,0,lctx, cleanup)) == NULL)  
+	    goto mem_error; 
+
+	
+	bt_poll_add(lctx->inquiry_fd, POLLIN, hci_inquiry_result, 0); 
+	insert_last(&ctx->list, s); 
+	DEBUGF("CMD_INQUIRY_START: Done");
+
+	goto ok;
+    }
+
+    case CMD_INQUIRY_STOP: { /* arguments: id:32 secs:32 */
+	uint32_t sid;
+
+	if (!ddata_get_uint32(&data_in, &sid))
+	    goto badarg;
+
+	// Will call cleanup, which will handle correct shutdown 
+	// of inquiry process.
+	remove_subscription(&ctx->list,INQUIRY,sid);
+
+	goto ok;
+    }
+
+    case CMD_RECENT_DEVICES: {  
+	DEBUGF("CMD_RECENT_DEVICES cmdid=%d: Not supported under linux", cmdid);
+	ddata_put_tag(&data_out, REPLY_OK);
+	ddata_put_UINT32(&data_out, cmdid);
+	ddata_put_tag(&data_out, LIST);
+	ddata_put_tag(&data_out, LIST_END);
+	goto reply;
+    }	
+
+    case CMD_PAIRED_DEVICES: {  
+	DEBUGF("CMD_PAIRED_DEVICES cmdid=%d: Not supported under linux", cmdid);
+	ddata_put_tag(&data_out, REPLY_OK);
+	ddata_put_UINT32(&data_out, cmdid);
+	ddata_put_tag(&data_out, LIST);
+	ddata_put_tag(&data_out, LIST_END);
+	goto reply;
+    }	
+
+    case CMD_FAVORITE_DEVICES: {  
+	DEBUGF("CMD_FAVORITE_DEVICES cmdid=%d: Not supported under linux", cmdid);
+	ddata_put_tag(&data_out, REPLY_OK);
+	ddata_put_UINT32(&data_out, cmdid);
+	ddata_put_tag(&data_out, LIST);
+	ddata_put_tag(&data_out, LIST_END);
+	goto reply;
+    }	
+
+    case CMD_DEVICE_INFO: {  
+	bdaddr_t bt_addr;
+	DEBUGF("CMD_DEVICE_INFO cmdid=%d", cmdid);
+
+	if(!get_address(&data_in, &bt_addr))
+	    goto badarg;
+
+	ddata_put_tag(&data_out, REPLY_OK);
+	ddata_put_UINT32(&data_out, cmdid);
+	ddata_put_tag(&data_out, LIST);
+	ddata_put_tag(&data_out, LIST_END);
+	
+	goto reply;
+    }	
+
+    case CMD_LOCAL_INFO: {  
+	DEBUGF("CMD_LOCAL_INFO cmdid=%d", cmdid);
+
+	ddata_put_tag(&data_out, REPLY_OK);
+	ddata_put_UINT32(&data_out, cmdid);
+
+	bt_local_info(lctx, &data_in, &data_out);
+	
+	goto reply;
+    }	
 
     default:
 	DEBUGF("CMD_UNKNOWN = %d cmdid=%d", op, cmdid);
@@ -405,7 +802,11 @@ void read_callback(struct pollfd* pfd, void* data)
     int fd = pfd->fd;
     int n;
 
-    DEBUGF("read_callback");
+    DEBUGF("read_callback: %d", pfd->fd);
+    if (pfd->revents & POLLHUP)  {
+	DEBUGF("hangup");
+	goto closed;
+    }
 
     if (ctx->pbuf_len < sizeof(ctx->pbuf)) {
 	int r = sizeof(ctx->pbuf) - ctx->pbuf_len;
@@ -460,7 +861,26 @@ closed:
 
 static void  main_loop(bt_ctx_t* ctx)
 {
-    (void) ctx;
+    int dev_id = 0;
+    int sock = 0;
+    drv_data_t local_context;
+    
+    // FIXME: How the hell do we handle more than one BT adapter.
+    dev_id = hci_get_route(NULL);
+    sock = hci_open_dev(dev_id);
+    if (sock == -1) {
+	fprintf(stderr, "Failed to open BT device %d: %s\n", dev_id, strerror(errno));
+	exit(0);
+    }
+    
+    // Setup local context
+    local_context.mgmt_sock = mgmt_open();
+    local_context.dev_id = dev_id;
+    local_context.dev_sock = sock;
+    local_context.inquiry_fd = -1;
+    local_context.inquiry_pid = -1;
+
+    ctx->drv_data = &local_context;
     
     while(1) {
 	int r = bt_poll(-1);
@@ -479,8 +899,8 @@ int main(int argc, char** argv)
     dlog_init();
     dlog_set_debug(DLOG_DEFAULT);
     memset(&bt_info, 0, sizeof(bt_ctx_t));
-
-    bt_poll_add(0, POLLIN, read_callback, &bt_info);
+    
+    bt_poll_add(0, POLLIN | POLLERR | POLLHUP, read_callback, &bt_info);
     main_loop(&bt_info);
 
     dlog_finish();
