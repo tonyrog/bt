1881db482be13a687f827d75095df337adb702a3
Tony Rogvall <tony@rogvall.se>
2021-12-29 22:51:35 +0100

    more tests

--- src/bt_send_wav.erl
+++ src/bt_send_wav.erl
@@ -9,21 +9,26 @@
 
 -export([send/2]).
 
+-define(PAYLOAD_TYPE, 96). %% SBC payload? what number 96????
+-define(SOURCE_NUM, 1).
+
 send(Address, WavFile) ->
     case file:open(WavFile, [read, raw, binary]) of
 	{ok,Fd} ->
 	    case alsa_wav:read_header(Fd) of
-		{ok, Header} ->
-		    io:format("wav header = ~p\n", [Header]),
+		{ok, Wav} ->
+		    io:format("wav header = ~p\n", [Wav]),
 		    {ok,PortList} = bt_sdp:protocol_port(Address, "AudioSink"),
 		    {value,{_,L2Port}} = lists:keysearch("L2CAP", 1, PortList),
 		    {value,{_,AVPort}} = lists:keysearch("AVDTP", 1, PortList),
+		    io:format("L2Port=~w, AVPort=~w\n", [L2Port,AVPort]),
+		    %% {ok, AVDTP} = l2cap:open(Address, AVPort),
 		    {ok, L2CAP} = l2cap:open(Address, L2Port),
-		    {ok, AVDTP} = l2cap:open(Address, AVPort),
-		    send_(Fd, AVDTP, L2CAP),
+		    AVDTP = false,
+		    send_(Fd, Wav, AVDTP, L2CAP),
 		    file:close(Fd),
 		    l2cap:close(L2CAP),
-		    l2cap:close(AVDTP),
+		    %%l2cap:close(AVDTP)
 		    ok;
 		Error ->
 		    file:close(Fd),
@@ -33,25 +38,52 @@ send(Address, WavFile) ->
 	    Error
     end.
 
-send_(Fd, AVDTP, L2CAP) ->
-    Enc = alsa_sbc:new(sbc),
-    {ok, _FrameLength} = alsa_sbc:get_frame_length(Enc),
-    {ok, FrameDuration} = alsa_sbc:get_frame_length(Enc),
+send_(Fd, Wav, AVDTP, L2CAP) ->
+    Rate = maps:get(rate, Wav),
+    Channels = maps:get(channels, Wav),
+    s16_le = maps:get(format, Wav), %% assert!
+    A2DP_Conf = [{rate, Rate},
+		 {channel_mode, 
+		  case Channels of
+		      2 -> stereo;
+		      1 -> mono
+		  end}],
+    io:format("a2dp conf = ~w\n", [A2DP_Conf]),
+    %% {ok,Enc} = alsa_sbc:new(msbc),
+    {ok,Enc} = alsa_sbc:new(a2dp, A2DP_Conf),
+    RtpSource = rtp:init_source(?PAYLOAD_TYPE, ?SOURCE_NUM, []),
+    {ok, SBCFrameLength} = alsa_sbc:get_frame_length(Enc),
+    {ok, FrameDurationUS} = alsa_sbc:get_frame_duration(Enc),
     {ok, CodeSize} = alsa_sbc:get_codesize(Enc),
-    send_(Fd, AVDTP, L2CAP, Enc, CodeSize, FrameDuration, <<>>).
+    {ok, Mtu} = bt_l2cap:get_mtu(L2CAP),
+    FramesPerSBCFrame = CodeSize div (Channels*2),
+    DurationPerSBCFrameUS = trunc((FramesPerSBCFrame*1000000)/Rate),
+    io:format("FrameLength ~w\n", [SBCFrameLength]),
+    io:format("FrameDuration ~wus\n", [FrameDurationUS]),
+    io:format("CodeSize ~w\n", [CodeSize]),
+    io:format("Mtu ~w\n", [Mtu]),
+    io:format("FramesPerSBCFrame=~w\n", [FramesPerSBCFrame]),
+    io:format("DurationPerSBCFrame=~wus\n", [DurationPerSBCFrameUS]),
+    send_(Fd, AVDTP, L2CAP, Enc, CodeSize, FramesPerSBCFrame, 
+	  DurationPerSBCFrameUS, RtpSource, <<>>).
 
-send_(Fd, AVDTP, L2CAP, Enc, CodeSize, FrameDuration, Data0) ->
+send_(Fd, AVDTP, L2CAP, Enc, CodeSize, FramesPerSBCFrame, 
+      DurationPerSBCFrameUS, RtpSource, Data0) ->
     case file:read(Fd, CodeSize) of
 	{ok, Data1} ->
 	    {ok,Fs,Data2} = alsa_sbc:encode(Enc, <<Data0/binary,Data1/binary>>),
-	    FRAG = 2#00000000, 
-	    NFrames = length(Fs),
-	    io:format("NFrames = ~w, duration = ~f ms\n", 
-		      [NFrames, (NFrames*FrameDuration)/1000]),
-	    l2cap:send(L2CAP, [(FRAG+NFrames), lists:reverse(Fs)]),
-	    timer:sleep(trunc((NFrames*FrameDuration)/1000)),
-	    send_(Fd, AVDTP, L2CAP, Enc, CodeSize, FrameDuration, Data2);
+	    FRAG = 2#00000000,
+	    NSBCFrames = length(Fs),
+	    NFrames = FramesPerSBCFrame*NSBCFrames,
+	    SBCPacket = iolist_to_binary([(FRAG+NSBCFrames),
+					  lists:reverse(Fs)]),
+	    {L2CapPacket,RtpSource1} = rtp:next(RtpSource,NFrames,SBCPacket),
+	    io:format("#frames = ~w, packet size = ~p\n", 
+		      [NFrames, byte_size(L2CapPacket)]),
+	    l2cap:send(L2CAP, L2CapPacket),
+	    timer:sleep(DurationPerSBCFrameUS div 1000),
+	    send_(Fd,AVDTP,L2CAP,Enc,CodeSize,FramesPerSBCFrame,
+		  DurationPerSBCFrameUS,RtpSource1,Data2);
 	eof ->
 	    ok
     end.
-    
